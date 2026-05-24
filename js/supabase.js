5daab5680c412e39fb0bdfb03dbe2b71b1348ef0
saboreincasa <carlosdinheironet@gmail.com>
2026-05-24 16:49:28 -0300

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = 'https://ejapatxehmxondjqsgvv.supabase.co'
+const SUPABASE_KEY = 'sb_publishable_B6_fpfgSxN56V2HoRQJCPg_ELaiatZr'
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+
